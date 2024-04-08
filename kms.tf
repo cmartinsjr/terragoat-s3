63cdae78031b9e91b8be368e4fcd4e184a11f09e
cmartinsjr <103141817+cmartinsjr@users.noreply.github.com>
2024-04-08 11:04:08 -0400

    Update kms.tf
    added new uuid

--- kms.tf
+++ kms.tf
@@ -5,6 +5,7 @@ resource "aws_kms_key" "logs_key" {
   deletion_window_in_days = 7
   tags = {
     owner = "cmartinsjr_paloaltonetworks_com"
+    yor_trace = "d18f5895-df92-4a5a-8cfb-6804237277ec"
   }
 }
 
